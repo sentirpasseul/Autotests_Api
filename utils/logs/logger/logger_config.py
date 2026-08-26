21eef1304032c1cce05f602b692bd6148332dacf
Dmitrii Evdokimov <ultra.las664@gmail.com>
2026-08-26 23:55:02 +0400

    Fixed logs config

--- utils/logs/logger/logger_config.py
+++ utils/logs/logger/logger_config.py
@@ -1,10 +1,12 @@
 import logging
 import os
+from pathlib import Path
 
 class LoggerConfig:
-    LOGS_DIR_NAME = "logs"
+    PROJECT_ROOT = Path(__file__).resolve().parents[3]
+    LOGS_DIR_NAME = PROJECT_ROOT / "artifacts" / "logs"
     LOGGER_NAME = "Logger"
-    LOGS_FILE_NAME = LOGS_DIR_NAME + os.sep + "test.log"
+    LOGS_FILE_NAME = LOGS_DIR_NAME / "test.log"
     LOGS_LEVEL = logging.INFO
     MAX_BYTES = 100000
     BACKUP_COUNT = 10
